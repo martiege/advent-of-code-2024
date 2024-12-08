a8c79c6e0d9eee97d79dec32b8339b34ad4c90b1
Martin Eek Gerhardsen <Martin.Eek.Gerhardsen@sintef.no>
2024-12-08 16:58:10 +0100

    kinda working example, given up for now...

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use itertools::Itertools;
 use nom::{
@@ -9,71 +9,68 @@ use nom::{
     sequence::separated_pair,
     IResult,
 };
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 advent_of_code::solution!(7);
 
-fn parse(input: &str) -> IResult<&str, Vec<(u32, Vec<u32>)>> {
+fn parse(input: &str) -> IResult<&str, Vec<(usize, Vec<u32>)>> {
     separated_list1(
         tag("\n"),
         separated_pair(
-            map_res(digit1, |s: &str| s.parse::<u32>()),
+            map_res(digit1, |s: &str| s.parse::<usize>()),
             tag(": "),
             separated_list0(tag(" "), map_res(digit1, |s: &str| s.parse::<u32>())),
         ),
     )(input)
 }
 
-#[derive(Debug, Hash, Eq, PartialEq, Clone)]
-enum Operations {
-    Plus,
-    Mult,
-}
+fn matches(sum: usize, vector: &[u32]) -> bool{
+    let mut results = HashSet::new();
+    results.insert(0);
+    results.insert(1);
 
-fn operations_folder(acc: Option<u32>, data: &((&u32, &u32), Operations)) -> Option<u32> {
-    match (acc, data) {
-        (None, ((a, b), Operations::Plus)) => Some(*a + *b),
-        (None, ((a, b), Operations::Mult)) => Some(*a * *b),
-        (Some(acc), ((_, b), Operations::Plus)) => Some(acc + *b),
-        (Some(acc), ((_, b), Operations::Mult)) => Some(acc * *b),
+    for v in vector {
+        let mut new_results = HashSet::new();
+        for r in results {
+            if r > sum {
+                continue;
+            }
+            let t = r + *v as usize;
+            new_results.insert(t);
+            let t = r * *v as usize;
+            new_results.insert(t);
+        }
+        results = new_results;
     }
+
+    results.contains(&sum)
 }
 
-fn get_all_operations<'a, T>(
-    data: &'a [(T, T)],
-) -> impl Iterator<Item = Vec<((T, T), Operations)>> + 'a
-where
-    T: Copy + 'a,
-{
-    [Operations::Plus, Operations::Mult]
-        .iter()
-        .combinations_with_replacement(data.len())
-        .flat_map(|v| v.into_iter().permutations(data.len()))
-        .unique()
-        .map(|x| {
-            data.iter()
-                .zip(x)
-                .map(|(n, c)| (*n, c.clone()))
-                .collect::<Vec<_>>()
-        })
-}
-
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<usize> {
     let data = parse(input);
 
-    if let Ok((_, data)) = data {
+    if let Ok((rest, data)) = data {
+        assert!(rest.trim().is_empty());
         Some(
-            data.iter()
-                .map(|(s, v)| {
-                    if get_all_operations(&v.iter().tuple_windows().collect_vec())
-                        .filter_map(|x| x.iter().fold(None, operations_folder))
-                        .any(|x| x == *s)
-                    {
-                        *s
-                    } else {
-                        0
-                    }
-                })
-                .sum(),
+            data
+            .par_iter()
+                .map(|(s, v)| if matches(*s, v) { *s } else {0} )
+                .sum()
+            // data
+            //     // .iter()
+            //     .par_iter()
+            //     .map(
+            //         |(s, v)| {
+            //         if get_all_operations(&v.iter().tuple_windows().collect_vec())
+            //             .filter_map(|x| operations_folder(*s, &x))
+            //             .any(|x| x == *s)
+            //         {
+            //             *s
+            //         } else {
+            //             0
+            //         }
+            //     })
+            //     .sum(),
         )
     } else {
         None
